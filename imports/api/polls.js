ac099ccbcfef729467a23a6e482a64c7c91f5868
Nuno Rocha <nunogcrocha@outlook.com>
2016-06-11 14:36:24 +0100

    cleanup

--- imports/api/polls.js
+++ imports/api/polls.js
@@ -12,9 +12,7 @@ if (Meteor.isServer) {
   });
   
   Meteor.publish('polls', function pollsPublication() {
-    return Polls.find({ 
-      //owner: this.userId 
-    });
+    return Polls.find({ });
   });
   
   Meteor.publish('poll', function(pollId) {
@@ -87,34 +85,5 @@ Meteor.methods({
     }
  
     Polls.remove(pollId);
-  },
-  /*
-  'polls.setChecked'(taskId, setChecked) {
-    check(taskId, String);
-    check(setChecked, Boolean);
- 
-    const task = Tasks.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      // If the task is private, make sure only the owner can check it off
-      throw new Meteor.Error('not-authorized');
-    }
-    
-    Tasks.update(taskId, { $set: { checked: setChecked } });
-  },
-  */
-  /*
-  'tasks.setPrivate'(taskId, setToPrivate) {
-    check(taskId, String);
-    check(setToPrivate, Boolean);
- 
-    const task = Tasks.findOne(taskId);
- 
-    // Make sure only the task owner can make a task private
-    if (task.owner !== Meteor.userId()) {
-      throw new Meteor.Error('not-authorized');
-    }
- 
-    Tasks.update(taskId, { $set: { private: setToPrivate } });
-  },
-  */
+  }
 });
